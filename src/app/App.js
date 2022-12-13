4bc4a80a2c3e1e970af4eb6a3a50747a51efec5e
Alwertus <alwertus@gmail.com>
2022-12-13 14:31:02 +0300

    Add tooltip

--- src/app/App.js
+++ src/app/App.js
@@ -3,7 +3,6 @@ import {useState} from "react";
 import {sendMsg} from "../common/SendMsg";
 import {ButtonComp} from "../common/components/Button/ButtonComp";
 import {ReactComponent as AccountIcon} from "../common/img/account.svg"
-import {ELEMENT_POSITION} from "../common/Structures";
 
 const App = () => {
     const [isAuthenticated, setIsAuthenticated] = useState(false)
@@ -12,26 +11,16 @@ const App = () => {
         sendMsg()
     }
 
-
     return <div className={style.wrapper}>
         <div className={style.topLine}>
             <div>TOP LINE</div>
             <ButtonComp
-                text={"ClickMe"}
+                tooltipText={"Login"}
                 onClick={loginOnClick}
                 icon={<AccountIcon/>}
             />
         </div>
-
-        <ButtonComp text={"ClickMe"}
-                    onClick={loginOnClick}
-                    icon={<AccountIcon/>}
-        />
-        <ButtonComp text={"ClickMe"}
-                    onClick={loginOnClick}
-                    icon={<AccountIcon/>}
-                    iconPosition={ELEMENT_POSITION.AFTER}
-        />
+        {isAuthenticated ? "AUTH: YES" : "AUTH: NO"}
 
     </div>
 }
--- src/common/components/Button/ButtonComp.js
+++ src/common/components/Button/ButtonComp.js
@@ -1,19 +1,29 @@
 import style from "./Button.module.css";
 import {ELEMENT_POSITION} from "../../Structures";
+import { Tooltip } from 'react-tooltip';
+import "react-tooltip/dist/react-tooltip.css";
+import {useId} from "react";
+
 
 export const ButtonComp = ({
     text,
     onClick,
     icon,
-    iconPosition = ELEMENT_POSITION.BEFORE
+    iconPosition = ELEMENT_POSITION.BEFORE,
+    tooltipText
 }) => {
-
     const reverseStyle = iconPosition === ELEMENT_POSITION.AFTER ? style.reverse : ""
+    const id = useId()
 
-    return <div className={style.wrapper} onClick={onClick}>
+    return <div id={id}
+        className={style.wrapper} onClick={onClick}>
         <div className={`${style.element} ${reverseStyle}`}>
             {icon}
             {text}
         </div>
+        {!!tooltipText && <Tooltip anchorId={id}
+                  place={"bottom"}
+                  content={tooltipText}
+        />}
     </div>
 }
