732195fc8ce88b86851fd29fa69c430360d0e71f
Alwertus <alwertus@gmail.com>
2023-04-30 16:48:09 +0600

    CashPage - set default tab = main

--- src/pages/Cash/ui/Cash.js
+++ src/pages/Cash/ui/Cash.js
@@ -3,16 +3,21 @@ import style from './Cash.module.css';
 import {CashGraph} from "../../../features/CashGraph";
 import {CashMenu} from "../../../features/CashMenu";
 import {CashMain} from "../../../widgets/CashMain";
-import {useParams} from "react-router-dom";
+import {useNavigate, useParams} from "react-router-dom";
 import {CashWallet} from "../../../widgets/CashWallet";
 
 export const Cash = ({savePath}) => {
     const {tab} = useParams()
+    const history = useNavigate()
     const tabContents = {main: <CashMain/>, wallet: <CashWallet/>}
 
     useEffect(() => {
         if (!!tab)
             savePath(tab)
+        else {
+            history("/cash/main")
+        }
+
     }, [tab])
 
     return <div className={style.wrapper}>
